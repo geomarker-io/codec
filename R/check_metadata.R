f7f8ededc1cdaa4582044d7ec4399b234771c166
erikarasnick <erika.rasnick@gmail.com>
2022-11-22 12:26:34 -0500

    check name

---
+++ R/check_metadata.R
@@ -0,0 +1,22 @@
+check_tdr_name <- function(name) {
+  # metadata has a name field
+  if(is.null(name)) stop("Metadata must have a field called 'name'.")
+  # name is a character string
+  if(!is.character(name)) stop("'name' must be character string.")
+  # name does not have uppercase letters
+  if(stringr::str_detect(name, "[[:upper:]]")) stop("'name' must be all lowercase.")
+  # name does not have spaces
+  if(stringr::str_detect(name, " ")) stop("'name' must not contain spaces.")
+  # nonalphanumeric characters are either -, _, or .
+  if(!all(stringr::str_detect(unlist(stringr::str_extract_all(name, "[^[:alnum:]]")), "[_.-]"))) {
+    stop("Accepted non-alphanumeric characters for 'name' are '-', '_', and '.'")
+  }
+}
+
+tdr <- read_tdr("https://codec-data.s3.amazonaws.com/census_mega_data/tabular-data-resource.yaml")
+check_tdr_name(tdr$name)
+check_tdr_name(tdr$erika)
+check_tdr_name(1)
+check_tdr_name("CensusData")
+check_tdr_name("census data")
+check_tdr_name("census+data")
