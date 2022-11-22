5eb92ad4db9d3c1b2e726030ccc7918525fa4d98
erikarasnick <erika.rasnick@gmail.com>
2022-11-22 12:52:17 -0500

    don't require download for test

--- tests/testthat/test-check_metadata.R
+++ tests/testthat/test-check_metadata.R
@@ -1,5 +1,6 @@
 test_that("check metadata name", {
-  tdr <- read_tdr("https://codec-data.s3.amazonaws.com/census_mega_data/tabular-data-resource.yaml")
+  tdr <- list()
+  tdr$name <- "census_data"
   expect_no_condition(check_tdr_name(tdr$name))
   expect_error(check_tdr_name(tdr$wrong))
   expect_error(check_tdr_name(1))
