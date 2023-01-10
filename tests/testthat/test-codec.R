bf85ae68f396384eaacf021d5e2e9040919eb606
Cole Brokamp <cole.brokamp@gmail.com>
2023-01-10 16:21:28 -0500

    delete read_codec test

--- tests/testthat/test-codec.R
+++ tests/testthat/test-codec.R
@@ -2,17 +2,6 @@ test_that("codec_tdr", {
   expect_snapshot(codec_tdr())
 })
 
-test_that("read_codec", {
-  skip("read_codec still experimental; don't download files for test for now")
-  skip_on_ci()
-  d <- read_codec("hh_acs_measures")
-  expect_identical(attr(d, "name"), "hh_acs_measures")
-  expect_true("census_tract_id" %in% names(d))
-  expect_true("census_tract_vintage" %in% names(d))
-  expect_true("year" %in% names(d))
-  fs::dir_delete("codec-data")
-  })
-
 test_that("check metadata name", {
   tdr <- list()
   tdr$name <- "census_data"
