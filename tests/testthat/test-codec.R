9a7f9d79b10030dc52c05be5149419f2805996b4
Cole Brokamp <cole.brokamp@gmail.com>
2023-01-12 23:01:14 -0500

    try some check functions for census tract id columns

--- tests/testthat/test-codec.R
+++ tests/testthat/test-codec.R
@@ -12,3 +12,28 @@ test_that("check metadata name", {
   expect_error(check_tdr_name("census data"))
   expect_error(check_tdr_name("census+data"))
 })
+
+test_that("check for census tract identifier", {
+
+  d <-
+    read_tdr_csv("https://github.com/geomarker-io/hamilton_drivetime/releases/download/v0.1.0") |>
+    dplyr::rename(census_tract_id_2010 = census_tract_id) |>
+    CODECtools::add_type_attrs()
+
+  expect_equal(check_census_tract_id(d), d)
+
+  expect_error({
+    .x |>
+      dplyr::select(-census_tract_id_2010) |>
+      check_census_tract_id()
+  },
+  regexp = "must contain a census tract id column called")
+
+  expect_error({
+    .x |>
+      dplyr::slice(-1) |>
+      check_census_tract_id()
+  },
+  regexp = "does not contain every census tract")
+                   
+})
