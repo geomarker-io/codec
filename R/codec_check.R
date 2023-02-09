22ea446f96cdb42a40b0855bb104591870edd9e8
Cole Brokamp <cole.brokamp@gmail.com>
2023-02-09 16:12:37 -0500

    make check doc point to vignette; closes #31

--- R/codec_check.R
+++ R/codec_check.R
@@ -1,145 +1,51 @@
 #' Check a tabular-data-resource against CODEC specifications
 #'
-#' This function will check that the file structure is correct,
+#' `check_codec_tdr_csv()` will use each of these functions to
+#' check that the file structure is correct,
 #' that the metadata can be read using CODEC specifications,
 #' that the CSV data can be read in with accompanying metadata,
 #' and that the data contains a census tract identifier column.
-#' @param .x path to folder containing the tdr
-#' @return tibble with added tabular-data-resource attributes
-#' (equivalent to read_tdr_csv with `codec = TRUE`)
+#' For more information on the CODEC specifications, see the
+#' vignette: `vignette("codec-specs", package = "CODECtools")`
+#' @param path path to tdr folder
+#' @param name the name field from tabular-data-resource.yaml
+#' @param tdr a codec tabular-data-resource
+#' @param tdr_md a codec tabular-data-resource metadata list object
+#' @return for `check_codec_tdr_csv`, a tibble with added
+#' tabular-data-resource attributes (equivalent to read_tdr_csv with `codec = TRUE`)
 #' @export
-check_codec_tdr_csv <- function(.x) {
-  check_files(.x)
-  tdr <- read_tdr(.x)$tdr
+check_codec_tdr_csv <- function(path) {
+  check_files(path)
+  tdr <- read_tdr(path)$tdr
   check_codec_tdr(tdr)
-  tdr_d <- read_tdr_csv(.x)
+  tdr_d <- read_tdr_csv(path)
   check_census_tract_id(tdr_d)
   return(invisible(tdr_d))
 }
 
-#' check CODEC tdr
-#' @param tdr a codec tabular-data-resource list object
-#' @return `tdr`, invisibly
-#' @export
-check_codec_tdr <- function(tdr) {
-
-  # must have "name" and "path" descriptors
-  if (!purrr::pluck_exists(tdr, "name")) stop("`name` property descriptor is required", call. = FALSE)
-  if (!purrr::pluck_exists(tdr, "path")) stop("`path` property descriptor is required", call. = FALSE)
-
-  # name must be valid
-  check_tdr_name(tdr$name)
-
-  # path must be valid
-  check_tdr_path(tdr$path)
-
-  # "name" must be identical to filename of "path" (without ".csv")
-  if (!tdr$name == fs::path_ext_remove(fs::path_file(tdr$path))) {
-    stop("name: ", tdr$name, " does not match ",
-      "file in path: ", tdr$path,
-      call. = FALSE
-    )
-  }
-
-  # all properties must be in codec specs
-  codec_property_names <- names(codec_tdr()$property)
-  codec_schema_names <- names(codec_tdr()$schema)
-  codec_fields_names <- names(codec_tdr()$fields)
-
-  tdr_property_names <- names(tdr)
-  tdr_schema_names <- names(tdr$schema)
-  tdr_fields_names <-
-    tdr$schema$fields |>
-    purrr::map(names) |>
-    unlist() |>
-    unique()
-
-  if (!all(tdr_property_names %in% codec_property_names)) {
-    stop("property descriptors not in the codec specification are not allowed: ",
-      paste(tdr_property_names[!tdr_property_names %in% codec_property_names], collapse = ", "),
-      call. = FALSE
-    )
-  }
-  if (!all(tdr_schema_names %in% codec_schema_names)) {
-    stop("schema descriptors not in the codec specification are not allowed: ",
-      paste(tdr_schema_names[!tdr_schema_names %in% codec_schema_names], collapse = ", "),
-      call. = FALSE
-    )
-  }
-  if (!all(tdr_fields_names %in% codec_fields_names)) {
-    stop("field descriptors not in the codec specification are not allowed: ",
-      paste(tdr_fields_names[!tdr_fields_names %in% codec_fields_names], collapse = ", "),
-      call. = FALSE
-    )
-  }
-
-  # TODO all descriptors should be neither empty or missing
-
-  return(invisible(tdr))
+#' Check data
+#' @rdname check_codec_tdr_csv
+check_data <- function(tdr) {
+  check_census_tract_id(tdr)
+  # check_date(.x)
 }
 
-
-#' check CODEC tdr name
-#'
-#' Name must be an identifier string composed of lower
-#' case alphanumeric characters, _, -, and .
-#' @param name the name field from tabular-data-resource.yaml
-#' @return NULL, invisibly
-#' @keywords internal
-check_tdr_name <- function(name) {
-  # name is a character string
-  if (!is.character(name)) stop("'name' must be character string.", call. = FALSE)
-  # name does not have uppercase letters
-  if (stringr::str_detect(name, "[[:upper:]]")) stop("'name' must be all lowercase.", call. = FALSE)
-  # name does not have spaces
-  if (stringr::str_detect(name, " ")) stop("'name' must not contain spaces.", call. = FALSE)
-  # nonalphanumeric characters are either -, _, or .
-  if (!all(stringr::str_detect(unlist(stringr::str_extract_all(name, "[^[:alnum:]]")), "[_.-]"))) {
-    stop("Accepted non-alphanumeric characters for 'name' are '-', '_', and '.'", call. = FALSE)
-  }
-  return(invisible(NULL))
-}
-
-#' check CODEC tdr path
-#' @keywords internal
-check_tdr_path <- function(path) {
-  # path is a character string
-  if (!is.character(path)) stop("'path' must be character string", call. = FALSE)
-  # path ends with .csv
-  if (! fs::path_ext(path) == "csv") stop("'path' must end with '.csv'", call. = FALSE)
-  # path can be a URL
-  if (is_url(path)) return(invisible(NULL))
-  # if not URL, check for absolute path
-  if (fs::is_absolute_path(path)) stop("'path' must be a relative file path")
-  # posix style path can't be enforced?
-  return(invisible(NULL))
-}
-
-#' Check for census tract id column
-#'
-#' Errors will be raised if the CODEC tabular data resource does not meet the following requirements:
-#' - data MUST contain a single census tract identifier column titled
-#' `census_tract_id_2000`, `census_tract_id_2010`, or `census_tract_id_2020`.
-#' - the census tract identifier column MUST contain all census tract identifiers
-#' in Hamilton County, OH for the appropriate vintage
-#'
-#' @param .x a codec tabular-data-resource
-#' @return .x, invisibly
-#' @keywords internal
-check_census_tract_id <- function(.x) {
+#' Check census tract id column
+#' @rdname check_codec_tdr_csv
+check_census_tract_id <- function(tdr) {
   census_tract_id_names <- paste0("census_tract_id", c("_2000", "_2010", "_2020"))
 
   # has census_tract_id_{year} or census_tract_id column
-  if (!any(names(.x) %in% census_tract_id_names)) {
+  if (!any(names(tdr) %in% census_tract_id_names)) {
     stop("must contain a census tract id column called census_tract_id_2000, census_tract_id_2010, or census_tract_id_2020", call. = FALSE)
   }
 
   # make sure only one tract column
-  if (sum(names(.x) %in% census_tract_id_names) > 1) {
+  if (sum(names(tdr) %in% census_tract_id_names) > 1) {
     stop("must contain only one census tract id column", call. = FALSE)
     }
 
-  census_tract_id_name <- census_tract_id_names[census_tract_id_names %in% names(.x)]
+  census_tract_id_name <- census_tract_id_names[census_tract_id_names %in% names(tdr)]
   census_tract_id_year <- stringr::str_extract(census_tract_id_name, "[0-9]+")
 
   required_census_tract_ids <-
@@ -147,7 +53,7 @@ check_census_tract_id <- function(.x) {
     eval() |>
     purrr::pluck(paste0("census_tract_id_", census_tract_id_year))
 
-  if (!all(required_census_tract_ids %in% .x[[census_tract_id_name]])) {
+  if (!all(required_census_tract_ids %in% tdr[[census_tract_id_name]])) {
     stop("the census tract id column, ",
       census_tract_id_name,
       ", does not contain every census tract in ",
@@ -156,24 +62,17 @@ check_census_tract_id <- function(.x) {
     )
   }
 
-  return(invisible(.x))
+  return(invisible(tdr))
 }
 
+#' Check date
+#' TODO
+
 #' Check files
-#'
-#' Errors will be raised if the CODEC tabular data resource does not meet the following requirements:
-#' - tdr directory must contain a `.csv` with the same name and a `tabular-data-resource.yaml` file
-#' - both files must use UTF-8 character encoding and have newlines encoded as `\n` or `\r\n`
-#' - the data file must:
-#'   - follow the [RFC 4180 standard](https://www.rfc-editor.org/rfc/rfc4180) for CSV files *and*
-#'   - have a header row containing a *unique* name for each field
-#'   - use `.` as the decimal mark (e.g., `1.34`)
-#'   - not use a grouping mark (e.g., `1200` instead of `1,200`)
-#' @param .x path to folder containing the tdr
-#' @return .x, invisibly
+#' @rdname check_codec_tdr_csv
 #' @export
-check_files <- function(.x) {
-  tdr_dir <- fs::path(.x)
+check_files <- function(path) {
+  tdr_dir <- fs::path(path)
   tdr_csv <- fs::path(tdr_dir, fs::path_file(tdr_dir), ext = "csv")
   tdr_yaml <- fs::path(tdr_dir, "tabular-data-resource.yaml")
 
@@ -217,5 +116,100 @@ check_files <- function(.x) {
     stop(tdr_csv, " could not be read without error:\n\n", test_read_csv_file$error, call. = FALSE)
   }
 
-  return(invisible(.x))
+  return(invisible(NULL))
 }
+
+#' check CODEC tdr
+#' @rdname check_codec_tdr_csv
+#' @export
+check_codec_tdr <- function(tdr_md) {
+
+  # must have "name" and "path" descriptors
+  if (!purrr::pluck_exists(tdr_md, "name")) stop("`name` property descriptor is required", call. = FALSE)
+  if (!purrr::pluck_exists(tdr_md, "path")) stop("`path` property descriptor is required", call. = FALSE)
+
+  # name must be valid
+  check_tdr_name(tdr_md$name)
+
+  # path must be valid
+  check_tdr_path(tdr_md$path)
+
+  # "name" must be identical to filename of "path" (without ".csv")
+  if (!tdr_md$name == fs::path_ext_remove(fs::path_file(tdr_md$path))) {
+    stop("name: ", tdr_md$name, " does not match ",
+      "file in path: ", tdr_md$path,
+      call. = FALSE
+    )
+  }
+
+  # all properties must be in codec specs
+  codec_property_names <- names(codec_tdr()$property)
+  codec_schema_names <- names(codec_tdr()$schema)
+  codec_fields_names <- names(codec_tdr()$fields)
+
+  tdr_property_names <- names(tdr_md)
+  tdr_schema_names <- names(tdr_md$schema)
+  tdr_fields_names <-
+    tdr_md$schema$fields |>
+    purrr::map(names) |>
+    unlist() |>
+    unique()
+
+  if (!all(tdr_property_names %in% codec_property_names)) {
+    stop("property descriptors not in the codec specification are not allowed: ",
+      paste(tdr_property_names[!tdr_property_names %in% codec_property_names], collapse = ", "),
+      call. = FALSE
+    )
+  }
+  if (!all(tdr_schema_names %in% codec_schema_names)) {
+    stop("schema descriptors not in the codec specification are not allowed: ",
+      paste(tdr_schema_names[!tdr_schema_names %in% codec_schema_names], collapse = ", "),
+      call. = FALSE
+    )
+  }
+  if (!all(tdr_fields_names %in% codec_fields_names)) {
+    stop("field descriptors not in the codec specification are not allowed: ",
+      paste(tdr_fields_names[!tdr_fields_names %in% codec_fields_names], collapse = ", "),
+      call. = FALSE
+    )
+  }
+
+  # TODO all descriptors should be neither empty or missing
+
+  return(invisible(tdr_md))
+}
+
+
+#' check CODEC tdr name
+#' @rdname check_codec_tdr_csv
+#' @export
+check_tdr_name <- function(name) {
+  # name is a character string
+  if (!is.character(name)) stop("'name' must be character string.", call. = FALSE)
+  # name does not have uppercase letters
+  if (stringr::str_detect(name, "[[:upper:]]")) stop("'name' must be all lowercase.", call. = FALSE)
+  # name does not have spaces
+  if (stringr::str_detect(name, " ")) stop("'name' must not contain spaces.", call. = FALSE)
+  # nonalphanumeric characters are either -, _, or .
+  if (!all(stringr::str_detect(unlist(stringr::str_extract_all(name, "[^[:alnum:]]")), "[_.-]"))) {
+    stop("Accepted non-alphanumeric characters for 'name' are '-', '_', and '.'", call. = FALSE)
+  }
+  return(invisible(NULL))
+}
+
+#' check CODEC tdr path
+#' @rdname check_codec_tdr_csv
+#' @export
+check_tdr_path <- function(path) {
+  # path is a character string
+  if (!is.character(path)) stop("'path' must be character string", call. = FALSE)
+  # path ends with .csv
+  if (! fs::path_ext(path) == "csv") stop("'path' must end with '.csv'", call. = FALSE)
+  # path can be a URL
+  if (is_url(path)) return(invisible(NULL))
+  # if not URL, check for absolute path
+  if (fs::is_absolute_path(path)) stop("'path' must be a relative file path")
+  # posix style path can't be enforced?
+  return(invisible(NULL))
+}
+
