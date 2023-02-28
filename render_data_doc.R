f3c7b7981c932bf1edc13b0db15f4a83fbfaae65
erikarasnick <erika.rasnick@gmail.com>
2023-02-28 13:34:35 -0500

    parameterized rmd for data documentation

---
+++ render_data_doc.R
@@ -0,0 +1,17 @@
+# make this a package function? how to render from inst folder?
+
+render_data_doc = function(data_name, data_version) {
+  rmarkdown::render(
+    "data_doc_template.Rmd", params = list(
+      data_name = data_name,
+      data_version = data_version
+    ),
+    output_file = glue::glue("doc-{data_name}-{data_version}.html")
+  )
+}
+
+render_data_doc(data_name = "hamilton_landcover",
+                data_version = "0.1.0")
+
+render_data_doc(data_name = "hamilton_traffic",
+                data_version = "0.1.0")
