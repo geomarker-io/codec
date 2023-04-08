a50353ef81dacb35f85131df5856e416a268bae8
Cole Brokamp <cole.brokamp@gmail.com>
2023-04-08 11:24:36 -0400

    fix path for data

--- inst/make_codec_data.R
+++ inst/make_codec_data.R
@@ -1,5 +1,5 @@
 library(codec)
 
 fs::path_package("codec") |>
-  fs::dir_ls(glob = "*codec_*.R") |>
+  fs::dir_ls(glob = "*hamilton_*.R") |>
   purrr::walk(callr::rscript, .progress = interactive())
