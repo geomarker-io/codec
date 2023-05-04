b924afc7147c5a57180a15f071267f4b6779c8e2
Cole Brokamp <cole.brokamp@gmail.com>
2023-05-04 11:55:27 -0400

    remove low_food_access_pop; closes #57

--- inst/hamilton_tract_indices.R
+++ inst/hamilton_tract_indices.R
@@ -8,6 +8,7 @@ d <-
     "{name}/releases/download/{version}/"
   )) |>
   dplyr::filter(substr(census_tract_id, 1, 5) == "39061") |>
+  dplyr::select(-low_food_access_pop) |>
   dplyr::rename(census_tract_id_2010 = census_tract_id) |>
   dplyr::mutate(year = as.integer(2019)) |>
   add_col_attrs(year, name = "year", title = "Year", description = "data year") |>
