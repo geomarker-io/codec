5c03ac108d289e57c5d5b41f18d2567487ed46a4
Cole Brokamp <cole.brokamp@gmail.com>
2023-10-13 22:56:36 -0400

    add back property code enforcement

--- inst/hamilton_property_code_enforcement.R
+++ inst/hamilton_property_code_enforcement.R
@@ -2,14 +2,34 @@ devtools::load_all()
 name <- "hamilton_property_code_enforcement"
 version <- "0.1.3"
 
-d <-
-  read_tdr_csv(glue::glue(
+rd <-
+  fr::read_fr_tdr(glue::glue(
     "https://github.com/geomarker-io/",
     "{name}/releases/download/{version}/"
-  )) |>
-  dplyr::mutate(year = as.integer(2022)) |>
-  add_col_attrs(year, name = "year", title = "Year", description = "data year") |>
-  add_type_attrs()
+  ))
 
-write_tdr_csv(d, fs::path_package("codec", "codec_data"))
+md <- as.list(rd)
+
+d <-
+  rd |>
+  tibble::as_tibble() |>
+  dplyr::mutate(year = 2022)
+
+d_tdr <-
+  purrr::reduce2(
+    names(d),
+    md$schema$fields[names(d)],
+    \(accum, xx, yy) fr::update_field(x = accum, field = xx, !!!yy),
+    .init = fr::as_fr_tdr(
+      d,
+      name = name,
+      version = version,
+      title = "Hamilton County Property Code Enforcement",
+      description = "Number of property code enforcements per household by census tract",
+      homepage = "https://geomarker.io/hamilton_property_code_enforcement"
+    )
+  ) |>
+  fr::update_field("year", title = "Year", description = "data year")
+
+fr::write_fr_tdr(d_tdr, fs::path_package("codec", "codec_data"))
 check_codec_tdr_csv(fs::path_package("codec", "codec_data", name))
