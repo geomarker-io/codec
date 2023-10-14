21f89e2dfd749f1c34c44ed27c65dd54b237405f
Cole Brokamp <cole.brokamp@gmail.com>
2023-10-13 23:02:11 -0400

    add tract_indices back

--- inst/hamilton_tract_indices.R
+++ inst/hamilton_tract_indices.R
@@ -2,18 +2,38 @@ devtools::load_all()
 name <- "tract_indices"
 version <- "v0.3.0"
 
-d <-
-  read_tdr_csv(glue::glue(
+rd <-
+  fr::read_fr_tdr(glue::glue(
     "https://github.com/geomarker-io/",
     "{name}/releases/download/{version}/"
-  )) |>
+  ))
+
+md <- as.list(rd)
+
+d <-
+  rd |>
+  tibble::as_tibble() |>
   dplyr::filter(substr(census_tract_id, 1, 5) == "39061") |>
   dplyr::select(-low_food_access_pop) |>
   dplyr::rename(census_tract_id_2010 = census_tract_id) |>
-  dplyr::mutate(year = as.integer(2019)) |>
-  add_col_attrs(year, name = "year", title = "Year", description = "data year") |>
-  add_type_attrs() |>
-  add_attrs(description = "A collection of census-derived indices for census tracts in Hamilton County")
+  dplyr::mutate(year = 2019)
 
-write_tdr_csv(d, fs::path_package("codec", "codec_data"))
+# create tdr by adding back in metadata for any
+# columns present in the original metadata
+d_tdr <-
+  purrr::reduce2(
+    names(d),
+    md$schema$fields[names(d)],
+    \(accum, xx, yy) fr::update_field(x = accum, field = xx, !!!yy),
+    .init = fr::as_fr_tdr(
+      d,
+      name = name,
+      version = version,
+      title = "Census Tract-Level Neighborhood Indices",
+      description = "A collection of census-derived indices for census tracts in Hamilton County",
+      homepage = "https://geomarker.io/tract_indices"
+    )
+  )
+
+fr::write_fr_tdr(d_tdr, fs::path_package("codec", "codec_data"))
 check_codec_tdr_csv(fs::path_package("codec", "codec_data", name))
