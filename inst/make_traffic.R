97248360f8372f492dd207e0fe0c0399711c52b8
Cole Brokamp <cole.brokamp@gmail.com>
2024-07-01 13:13:56 -0400

    "standalone" traffic data calculations

---
+++ inst/make_traffic.R
@@ -0,0 +1,49 @@
+
+
+dest_path <- tempfile(fileext = ".gdb.zip")
+
+"https://www.arcgis.com/sharing/rest/content/items/c199f2799b724ffbacf4cafe3ee03e55/data" |>
+  utils::download.file(dest_path, mode = "wb", method = "wget")
+
+sf::st_layers(dsn = dest_path)$name |>
+  strsplit("_", fixed = TRUE) |>
+  purrr::map_chr(3)
+
+
+rd <-
+  sf::st_read(
+    dsn = dest_path,
+    query = paste(
+      "SELECT F_SYSTEM, AADT, AADT_SINGLE_UNIT, AADT_COMBINATION",
+      "FROM HPMS_FULL_OH_2020",
+      "WHERE F_SYSTEM IN ('1', '2', '3')"
+    ),
+    quiet = FALSE
+  )
+
+aadt <-
+  rd |>
+  sf::st_transform(sf::st_crs(cincy::tract_tigris_2020)) |>
+  sf::st_zm()
+
+out <-
+  sf::st_join(cincy::tract_tigris_2020, aadt) |>
+  sf::st_drop_geometry() |>
+  tibble::as_tibble() |>
+  dplyr::group_by(census_tract_id_2020) |>
+  dplyr::summarize(
+    aadt = sum(AADT, na.rm = TRUE),
+    aadt_truck = sum(AADT_SINGLE_UNIT, AADT_COMBINATION, na.rm = TRUE)
+  )
+
+out |>
+  dplyr::mutate(year = 2020) |>
+  fr::as_fr_tdr(
+    name = "codec_traffic",
+    title = "Average Annual Daily Truck and Total Traffic Counts"
+  ) |>
+  fr::update_field("aadt", title = "Average Annual Daily Traffic (total)") |>
+  fr::update_field("aadt_truck", title = "Average Annual Daily Traffic (trucks only)") |>
+  fr::write_fr_tdr(fs::path_package("codec"))
+
+check_codec_tdr_csv(fs::path_package("codec", "codec_traffic"))
