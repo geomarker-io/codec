4914eca7e9f02f82cfaa6033e7e131d4ecc42410
Cole Brokamp <cole.brokamp@gmail.com>
2024-08-07 17:04:40 -0400

    hh_acs_measures done

---
+++ inst/hh_acs_measures/README.md
@@ -0,0 +1,3 @@
+# Harmonized Historical American Community Survey Measures
+
+Taken as the Hamilton County, OH subset of https://github.com/geomarker-io/hh_acs_measures
---
+++ inst/hh_acs_measures/hh_acs_measures.R
@@ -0,0 +1,20 @@
+rd <- fr::read_fr_tdr("https://github.com/geomarker-io/hh_acs_measures/releases/download/v1.1.0/")
+
+out <-
+  rd |>
+  fr::fr_filter(substr(census_tract_id, 1, 5) == "39061") |>
+  fr::fr_select(-census_tract_vintage) |>
+  fr::fr_rename(census_tract_id_2010 = census_tract_id)
+
+
+out_dpkg <-
+  out |>
+  as_codec_dpkg(
+    name = "hh_acs_measures",
+    version = "1.1.0",
+    title = "Harmonized Historical American Community Survey Measures",
+    homepage = "https://github.com/geomarker-io/hh_acs_measures",
+    description = paste(readLines(fs::path_package("codec", "hh_acs_measures", "README.md")), collapse = "\n")
+  )
+
+codec_dpkg_s3_put(out_dpkg)
