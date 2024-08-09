0816a0a913cf146909b958dcd75f4f1e666a416f
Cole Brokamp <cole.brokamp@gmail.com>
2024-08-09 14:22:13 -0400

    landcover; delete others to be completed in other PRs

---
+++ inst/landcover/landcover.R
@@ -0,0 +1,22 @@
+devtools::load_all()
+rd <- fr::read_fr_tdr("https://github.com/geomarker-io/hamilton_landcover/releases/download/v0.1.0/")
+
+out <-
+  rd |>
+  tibble::as_tibble() |>
+  dplyr::rename(enhanced_vegatation_index_2018 = evi_2018,
+                census_tract_id_2010 = census_tract_id) |>
+  dplyr::mutate(year = 2019)
+
+out_dpkg <-
+  out |>
+  as_codec_dpkg(
+    name = "landcover",
+    version = "0.1.0",
+    title = "Landcover, Built Environment, and Greenness",
+    homepage = "https://github.com/geomarker-io/hamilton_landcover",
+    description = paste(readLines(fs::path_package("codec", "landcover", "README.md")), collapse = "\n")
+  )
+
+dpkg::dpkg_meta(out_dpkg)
+codec_dpkg_s3_put(out_dpkg)
--- inst/tract_indices/README.md
+++
@@ -1,3 +0,0 @@
-# 
-
-Taken as the Hamilton County, OH subset of https://github.com/geomarker-io/tract_indices
