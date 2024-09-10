0acb698b21621a1d996ae044867076e35ef5c3f1
erikarasnick <erika.rasnick@gmail.com>
2024-09-10 14:14:35 -0400

    year-month-tract

--- inst/codec_data/xx_address/xx_address.R
+++ inst/codec_data/xx_address/xx_address.R
@@ -11,40 +11,51 @@ library(geoarrow)
 options(arrow.unsafe_metadata = TRUE)
 
 crime_incidents <-
-  dpkg::stow("https://github.com/geomarker-io/xx_address/releases/download/crime_incidents-v0.1.0/crime_incidents-v0.1.0.parquet") |>
+  dpkg::stow("https://github.com/geomarker-io/xx_address/releases/download/crime_incidents-v0.1.1/crime_incidents-v0.1.1.parquet") |>
   arrow::read_parquet() |>
   mutate(geometry = sf::st_as_sfc(geometry)) |>
   st_as_sf(crs = 4326) |>
   st_transform(st_crs(cincy::tract_tigris_2010)) |>
-  st_join(cincy::tract_tigris_2010) |>
+  st_join(cincy::tract_tigris_2010, largest = TRUE) |>
   st_drop_geometry() |>
-  select(-tlid) |>
-  group_by(census_tract_id_2010) |>
-  summarize(across(c(property, violent, other), \(x) sum(x, na.rm = TRUE)))
+  mutate(
+    year = lubridate::year(date_time), 
+    month = lubridate::month(date_time)
+      ) |>
+  select(-tlid, -address_x, -date_time) |>
+  group_by(census_tract_id_2010, year, month, category) |>
+  tally()|>
+  tidyr::pivot_wider(
+    names_from = category, 
+    values_from = n
+  )
 
 shotspotter <-
-  dpkg::stow("https://github.com/geomarker-io/xx_address/releases/download/shotspotter-v0.1.0/shotspotter-v0.1.0.parquet") |>
+  dpkg::stow("https://github.com/geomarker-io/xx_address/releases/download/shotspotter-v0.1.1/shotspotter-v0.1.1.parquet") |>
   arrow::read_parquet() |>
   mutate(geometry = sf::st_as_sfc(geometry)) |>
   st_as_sf(crs = 4326) |>
   st_transform(st_crs(cincy::tract_tigris_2010)) |>
-  st_join(cincy::tract_tigris_2010) |>
+  st_join(cincy::tract_tigris_2010, largest = TRUE) |>
   st_drop_geometry() |>
-  select(-tlid) |>
-  group_by(census_tract_id_2010) |>
-  summarize(gunshots = sum(gunshots, na.rm = TRUE))
+  mutate(
+    year = lubridate::year(date_time), 
+    month = lubridate::month(date_time)
+    ) |>
+  select(-tlid, -address_x, -date_time) |>
+  group_by(census_tract_id_2010, year, month) |>
+  tally() |>
+  rename(gunshots = n)
 
 d_out <-
-  cincy::tract_tigris_2010 |>
-  st_drop_geometry() |>
-  as_tibble() |>
-  left_join(crime_incidents, by = "census_tract_id_2010") |>
-  left_join(shotspotter, by = "census_tract_id_2010") |>
-  mutate(across(c(property, violent, other, gunshots), \(x) ifelse(is.na(x), 0, x))) |>
-  filter(!is.na(census_tract_id_2010)) |>
-  mutate(year = "2024")
-
-mapview::mapview(left_join(cincy::tract_tigris_2010, d_out), zcol = "property")
+  full_join(
+    crime_incidents, 
+    shotspotter, 
+    by = c("census_tract_id_2010", "year", "month")) |>
+  mutate(
+    across(c(property, violent, other, gunshots), 
+    \(x) ifelse(is.na(x), 0, x))) |>
+  filter(!is.na(census_tract_id_2010))
 
 out_dpkg <-
   d_out |>
