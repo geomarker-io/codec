e2926f5a2783639985d0e5b42b1e9d7d6db7db59
Cole Brokamp <cole.brokamp@gmail.com>
2024-12-24 10:49:34 -0500

    more interpolate wip

--- R/cincy_interpolate.R
+++ R/cincy_interpolate.R
@@ -11,7 +11,7 @@
 #' @returns a simple features object with a geographic identifier column (`geoid`)
 #' and a geometry column (`s2_geography`) in addition to the (interpolated) columns in `from`
 #' @examples
-#' codec_interpolate(get_codec_dpkg("property_code_enforcements-v0.2.0"))
+#' codec_interpolate(from = get_codec_dpkg("property_code_enforcements-v0.2.0"))
 #' codec_interpolate(get_codec_dpkg("property_code_enforcements-v0.2.0"), to = "zcta")
 codec_interpolate <- function(from, to = NULL, weights = c("pop", "homes", "area")) {
   weights <- rlang::arg_match(weights)
@@ -46,47 +46,53 @@ cincy_block_weights <- function() {
   return(out)
 }
 
+from <-
+  cincy_census_geo("tract", "2020") |>
+  sf::st_transform(5072) |>
+  dplyr::mutate(n = 0.1)
+to <- sf::st_transform(cincy_zcta_geo("2020"), 5072)
 
-## from <-
-##   cincy_census_geo("tract", "2020") |>
-##   sf::st_transform(5072) |>
-##   dplyr::mutate(n = 1)
+bw <-
+  cincy_block_weights() |>
+  sf::st_transform(5072) |>
+  dplyr::select(weight = pop, s2_geography)
+  
+# from 'total weights'
+fromm <-
+  sf::st_join(from, bw, left = FALSE) |>
+  sf::st_drop_geometry() |>
+  dplyr::summarize(total_weight = sum(weight, na.rm = TRUE), .by = "geoid") |>
+  dplyr::right_join(from, by = "geoid") |>
+  sf::st_as_sf()
 
-## bw <- sf::st_transform(cincy_block_weights(), 5072)
+# calculate intersections and intersection proportions
+intersections <-
+  fromm |>
+  sf::st_intersection(to) |>
+  dplyr::filter(sf::st_is(s2_geography, c("POLYGON", "MULTIPOLYGON", "GEOMETRYCOLLECTION"))) |>
+  dplyr::mutate(intersection_id = dplyr::row_number())
 
-## total_weights <-
-##   sf::st_join(from, bw, left = FALSE) |>
-##   sf::st_drop_geometry() |>
-##   dplyr::summarize(total_weights = sum(pop, na.rm = TRUE), .by = "geoid")
+weights <-
+  sf::st_join(bw, intersections) |>
+  sf::st_drop_geometry() |>
+  dplyr::summarize(
+    weight = sum(weight, na.rm = TRUE),
+    weight_coef = weight / total_weight,
+    .by = "intersection_id"
+  ) |>
+  dplyr::distinct(weight_coef, .keep_all = TRUE) |>
+  dplyr::right_join(intersections, by = "intersection_id") |>
+  dplyr::select(geoid, geoid.1, weight, weight_coef)
 
-## from <- dplyr::left_join(from, total_weights, by = "geoid")
+# non-extensive (same total)
+fromm |>
+  sf::st_drop_geometry() |>
+  dplyr::left_join(weights, by = "geoid") |>
+  dplyr::summarize(n_interpolated = sum(n * weight_coef, na.rm = TRUE), .by = "geoid.1")
 
-## to <- sf::st_transform(cincy_zcta_geo("2020"), 5072)
+# extensive (same average)
+fromm |>
+  sf::st_drop_geometry() |>
+  dplyr::left_join(weights, by = "geoid") |>
+  dplyr::summarize(n_interpolated = weighted.mean(n, weight, na.rm = TRUE), .by = "geoid.1")
 
-## from_to_int <-
-##   sf::st_intersection(from, to) |>
-##   suppressWarnings() |>
-##   dplyr::mutate(.row = dplyr::row_number()) |>
-##   sf::st_join(bw, left = FALSE)
-
-## intersections <-
-##   from_to_int |>
-##   sf::st_drop_geometry() |>
-##   dplyr::mutate(intersection_value = sum(pop, na.rm = TRUE), .by = ".row") |>
-##   dplyr::distinct(.row, .keep_all = TRUE) |>
-##   dplyr::mutate(weight_coef = intersection_value / total_weights.x) |>
-##   dplyr::select(-.row, -pop, -homes, -area)
-
-## to_non_extensive <-
-##   from |>
-##   sf::st_drop_geometry() |>
-##   tibble::as_tibble() |>
-##   dplyr::select(geoid) |>
-##   dplyr::left_join(intersections, by = "geoid") |>
-##   dplyr::summarize(
-##     dplyr::across(
-##       tidyselect::vars_select_helpers$where(is.numeric),
-##       .fns = ~ weighted.mean(.x, w = intersection_value, na.rm = TRUE)
-##     ),
-##     .by = "geoid.1"
-##   )
