166337ffcfe4f43d8a88e68e4d2468abde60fe47
Cole Brokamp <cole.brokamp@gmail.com>
2025-08-27 22:18:29 -0400

    as_codec_tbl takes what we need from dpkg; use pins

--- tests/testthat/test-codec_check.R
+++ tests/testthat/test-codec_check.R
@@ -7,12 +7,16 @@ test_that("check census tract id", {
   readRDS(testthat::test_path("drivetime", "drivetime.rds")) |>
     dplyr::rename(census_tract_id = census_tract_id_2010) |>
     check_census_tract_id() |>
-    expect_identical("must contain one census tract id column called census_tract_id_2010 or census_tract_id_2020")
+    expect_identical(
+      "must contain one census tract id column called census_tract_id_2010 or census_tract_id_2020"
+    )
 
   readRDS(testthat::test_path("drivetime", "drivetime.rds")) |>
     dplyr::slice_head(n = 5) |>
     check_census_tract_id() |>
-    expect_identical("the census tract id column, census_tract_id_2010, does not contain every census tract for that vintage; Check for missing census tract observations and check that you are using the correct vintage.")
+    expect_identical(
+      "the census tract id column, census_tract_id_2010, does not contain every census tract for that vintage; Check for missing census tract observations and check that you are using the correct vintage."
+    )
 })
 
 test_that("check date", {
@@ -26,11 +30,39 @@ test_that("check date", {
 
   tibble::tibble(year = "foofy") |>
     check_date() |>
-    expect_equal("the 'year' field must only contain integer years between 1970 and 2099")
+    expect_equal(
+      "the 'year' field must only contain integer years between 1970 and 2099"
+    )
 
   tibble::tibble(year = 2000, month = 14) |>
     check_date() |>
     expect_equal("the 'month' field  must only contain integer values 1-12")
+})
+
+test_that("as_codec_tbl works", {
+  readRDS(testthat::test_path("drivetime", "drivetime.rds")) |>
+    dplyr::mutate(year = 2021) |>
+    as_codec_tbl(
+      name = "foofy",
+      description = "# My foofy data set \n This is a very cool data table."
+    ) |>
+    expect_s3_class("codec_tbl")
+
+  readRDS(testthat::test_path("drivetime", "drivetime.rds")) |>
+    as_codec_tbl(
+      name = "foofy",
+      description = "# My foofy data set \n This is a very cool data table."
+    ) |>
+    expect_error("contain a 'year' column")
+
+  readRDS(testthat::test_path("drivetime", "drivetime.rds")) |>
+    dplyr::slice_head(n = 5) |>
+    dplyr::mutate(year = 2021) |>
+    as_codec_tbl(
+      name = "foofy",
+      description = "# My foofy data set \n This is a very cool data table."
+    ) |>
+    expect_error("does not contain every census tract")
 })
 
 test_that("as_codec_dpkg works", {
