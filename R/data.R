58302111609f7044c87c5074d83eb7b5d175bf6e
Cole Brokamp <cole.brokamp@gmail.com>
2025-09-03 15:21:33 -0400

    add examples to doc for codec latest annual

--- R/data.R
+++ R/data.R
@@ -3,6 +3,9 @@
 #' CoDEC data is harmonized as the most recently available annual (or annual average of monthly) values
 #' at the census tract 2020 geography.
 #' The year represents the year this table was assembled and mainly used as a placeholder to this object
-#' can be used with functions that take a codec_tbl object (see `?as_codec_tbl).
+#' can be used with functions that take a codec_tbl object (see `?as_codec_tbl`).
 #' See the description metadata for the actual latest years used for each CoDEC table.
+#' @examples
+#' glue::glue(attr(codec_latest_annual, "description"))
+#' tibble::glimpse(codec_latest_annual)
 "codec_latest_annual"
